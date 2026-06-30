c0eeac2084d1959227143ea3a1ebe75cbe7fccd3
psychonimus <prafulshah3210@gmail.com>
2026-06-30 15:42:38 +0530

    new fixes

--- src/components/Pages/SellPage/SellPage.jsx
+++ src/components/Pages/SellPage/SellPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SellPageHero from './SellPagehero/SellPageHero'
+import SellPageHero from './SellPageHero/SellPageHero'
 import SellPageForm from './SellPageForm/SellPageForm'
 
 
