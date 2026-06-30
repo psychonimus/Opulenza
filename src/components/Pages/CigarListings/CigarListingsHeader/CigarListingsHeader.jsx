e48e27aba6d6d83dbae958cd000a5e1f80ab904b
psychonimus <prafulshah3210@gmail.com>
2026-06-30 15:31:23 +0530

    module fix

--- src/components/Pages/CigarListings/CigarListingsHeader/CigarListingsHeader.jsx
+++ src/components/Pages/CigarListings/CigarListingsHeader/CigarListingsHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../../../Header/Header'
-import "./cigarListingsHeader.css"
+import "./CigarListingsHeader.css"
 
 const CigarListingsHeader = () => {
   return (
